160853de8711d6b9e534d44de6bac31e572d54d2
mryork <me@myork.net>
2019-12-10 18:07:54 -0500

    Finished backend for post manipulation.

--- backend/index.js
+++ backend/index.js
@@ -23,51 +23,110 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('./frontend'))
 
 var user = mongoose.model('User', new mongoose.Schema({ id: String, name: String }))
-var post = mongoose.model('Post', new mongoose.Schema({ department: String, number: String, materials: String, sold: Boolean, user: String, userName: String }))
-var inquiry = mongoose.model('Inquiry', mongoose.Schema({ post: String, body: String, date: String, from: String}))
 
+// Post (Item)
+var post = mongoose.model('Post', new mongoose.Schema({
+    name: String,           // Post Name
+    description: String,    // Post Description
+    date: Number,           // Posted Date (millis format)
+    department: String,     // Department (_ _ _ _)
+    number: String,         // Course Number
+    price: Number,          // Price of Item
+    userName: String,       // Name of User
+    userID: String,         // Google ID of User
+    sold: Boolean,          // Sold or Not
+    googlePic: String       // User Picture from Google
+}))
+
+// Inquiry (Asking)
+var inquiry = mongoose.model('Inquiry', mongoose.Schema({
+    message: String,        // Body of Inquiry
+    phoneNumber: String,    // User's Provided Phone #
+    email: String,          // User's Provided Email
+    postName: String,       // Name of Post Inquired
+    userFrom: String,       // From's Name
+    userTo: String,         // To's ID
+    date: Number            // Date Sent
+}))
+
+// Status Checker
 app.get("/status", (req, res) => {
     res.status(200);
     res.send("The Heel Me Down server started successfully.");
 })
 
-/**
- * Search API Endpoint
- * 
- * Returns a list of posts based on search 
- */
+// Search (params: department, number)
 app.post("/api/search", (req,res) => {
-    const department = req.body.dept;
-    const number = req.body.number;
+    if(req.body.number == undefined &&
+        req.body.department != undefined) {
 
-    post.find({ department: department, number: number }).then((vals) => {
-        console.log(vals);
-        res.json(vals);
-        res.status(200);
-        res.send();
+        post.find({ department: req.body.department }).then(vals => {
+            res.json(vals)
+            res.status(200)
+            res.send()
+        })
+    } else if (req.body.department == undefined) {
+        post.find().then(vals => {
+            res.json(vals)
+            res.status(200)
+            res.send()
+        })
+    } else {
+        post.find({ department: req.body.department, number: req.body.number}).then(vals => {
+            res.json(vals)
+            res.status(200)
+            res.send()
+        })
+    }
+})
+
+// Logged in User's Posts
+app.post("/api/userPosts", (req,res) => {
+    const token = req.body.token;
+
+    verifyUser(token).then((obj) => {
+        post.find({userID: obj['sub']}).then((p) => {
+            res.json(p);
+            res.status(200);
+            res.send();
+        }).catch((e) => {
+            console.log(e);
+            res.sendStatus(400);
+        })
+    }).catch((e) => {
+        console.log(e)
+        res.sendStatus(400);
     })
 })
 
-app.post("/api/post", (req,res) => {
+// Create Post (params: name, description, department, number, price)
+app.post("/api/createPost", (req,res) => {
     const token = req.body.token;
     const name = req.body.name;
+    const description = req.body.description;
     const department = req.body.department;
     const number = req.body.number;
-    const materials = req.body.materials;
+    const date = Date.now();
+    const userName = req.body.userName;
     const sold = false;
 
-    verifyUser(token).then((id) => {
-        user.findOne({id: id}).then((u) => {
+    verifyUser(token).then((obj) => {
+        console.log(obj)
+        user.findOne({id: obj['sub']}).then((u) => {
             if(!u) {
                 user.create({id: id, name: name}).then((u) => {
-                    post.create({ userName: name, department: department, number: number, materials: materials, sold: sold, user: u.id}).then(() => {
+                    post.create({ name: name, description: description, department: department,
+                        number: number, userName: userName, sold: sold,
+                        userID: u.id, date: date, googlePic: obj['picture'] }).then((p) => {
                         res.sendStatus(200);
                     }).catch(() => {
                         res.sendStatus(400);
                     })
                 })
             } else {
-                post.create({ userName: name, department: department, number: number, materials: materials, sold: sold, user: u.id}).then(() => {
+                post.create({ name: name, description: description, department: department,
+                    number: number, userName: userName, sold: sold,
+                    userID: u.id, date: date, googlePic: obj['picture'] }).then(() => {
                     res.sendStatus(200);
                 }).catch(() => {
                     res.sendStatus(400);
@@ -77,14 +136,15 @@ app.post("/api/post", (req,res) => {
     })
 })
 
+// Item Was Not Sold
 app.post("/api/unsold", (req,res) => {
     const token = req.body.token;
     const postID = req.body.id;
 
-    verifyUser(token).then((id) => {
-        post.findOne({id: postID, user: id}).then((p) => {
-            if(!u) {
-                res.sendStatus(401);
+    verifyUser(token).then((obj) => {
+        post.findOne({_id: postID, userID: obj['sub']}).then((p) => {
+            if(!p) {
+                res.sendStatus(400);
             } else {
                 p.sold = false;
                 p.save();
@@ -99,47 +159,45 @@ app.post("/api/unsold", (req,res) => {
     })
 })
 
+// Item Was Sold
 app.post("/api/sold", (req,res) => {
     const token = req.body.token;
     const postID = req.body.id;
 
-    verifyUser(token).then((id) => {
-        post.findOne({id: postID, user: id}).then((p) => {
-            if(!u) {
-                res.sendStatus(401);
+    verifyUser(token).then((obj) => {
+        post.findOne({_id: postID, userID: obj['sub']}).then((p) => {
+            if(!p) {
+                res.sendStatus(400);
             } else {
                 p.sold = true;
                 p.save();
 
                 res.sendStatus(200);
             }
-        }).catch(() => {
+        }).catch((e) => {
+            console.log(e);
             res.sendStatus(400);
         })
-    }).catch(() => {
+    }).catch((e) => {
+        console.log(e)
         res.sendStatus(400);
     })
 })
 
-app.post("/api/sold", (req,res) => {
+// Delete a Post
+app.post("/api/deletePost", (req,res) => {
     const token = req.body.token;
     const postID = req.body.id;
-    const materials = req.body.materials;
 
-    verifyUser(token).then((id) => {
-        post.findOne({id: postID, user: id}).then((p) => {
-            if(!u) {
-                res.sendStatus(401);
-            } else {
-                p.materials = materials;
-                p.save();
-
-                res.sendStatus(200);
-            }
-        }).catch(() => {
+    verifyUser(token).then((obj) => {
+        post.deleteOne({_id: postID, userID: obj['sub']}).then((p) => {
+            res.sendStatus(200);
+        }).catch((e) => {
+            console.log(e);
             res.sendStatus(400);
         })
-    }).catch(() => {
+    }).catch((e) => {
+        console.log(e)
         res.sendStatus(400);
     })
 })
@@ -157,7 +215,6 @@ async function verifyUser(token) {
     });
 
     const payload = ticket.getPayload();
-    const userid = payload['sub'];
 
-    return userid;
+    return payload;
 }
